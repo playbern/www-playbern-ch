6c03252b71a45f57e3454a80e517d607e24da7a0
Halil-Kuray <71791007+Halil-Kuray@users.noreply.github.com>
2023-08-16 12:48:21 +0200

    utol Section colors adjusted

--- components/util/section.tsx
+++ components/util/section.tsx
@@ -19,7 +19,7 @@ export const Section = ({ children, color = "", className = "" }) => {
       orange:
         "text-white bg-orange-500 bg-gradient-to-br from-orange-500 to-orange-600",
       yellow:
-        "text-gray-1000 bg-gray-200 bg-gradient-to-br from-yellow-400 to-yellow-400",
+      "text-gray-1000 bg-gray-200 bg-yellow-400",
     },
   };
   const sectionColorCss =
